ffa937ed96dcf4d1aaba80becafcecc94dc412da
Rishi2481007 <rpatel0502@conestogac.on.ca>
2024-07-24 21:50:22 -0400

    Adding Bash Scripts

---
+++ Week12/myScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# A simple script that uses variables - Rishi Patel
+
+echo "I am logged into the $USER account."
+echo "The Filename for this script is $0"
+echo "The Input for this file is $1"
+
+exit
